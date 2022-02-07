e5c5854071970fc75f52f07cf96c731d6ad3d734
Kaio Delphino <kaio@tutamail.com>
2022-02-07 19:09:49 -0300

    test: add character entity tests

---
+++ test/domain/entities/character_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:star_wars_app/domain/entities/character.dart';
+
+import './people.dart' as json;
+
+
+void main() {
+  test("Should return a object with luke name", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+    expect(character.name, "Luke Skywalker");
+  });
+
+  test("Should return luke homeworld as string", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+
+    expect(character.homeworld.runtimeType, String);
+  });
+
+  test("Should return luke homeworld as empty instead of url on instantiation from json", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+
+    expect(character.homeworld, "");
+  });
+
+  test("Should return luke correct homeworld name when setHomeworld", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+    character.setHomeworld("Tatooine");
+
+    expect(character.homeworld, "Tatooine");
+  });
+
+  test("Should not be able to set homeworld name twice", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+    character.setHomeworld("Tatooine");
+    final wasAbleToSet = character.setHomeworld("Tatooine");
+
+    expect(wasAbleToSet, false);
+  });
+
+  test("Should return luke species as n/a", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+
+    expect(character.species[0], "n/a");
+  });
+
+  test("Should return r2d2 hair color as n/a", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+
+    expect(character.hairColor, "n/a");
+  });
+
+  test("Should return r2d2 height as double", () {
+    final people = json.peopleResponse["results"];
+    final character = Character.fromMap(people[0]);
+
+    expect(character.height.runtimeType, double);
+  });
+}
+
