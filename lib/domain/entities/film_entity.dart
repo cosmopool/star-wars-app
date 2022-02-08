bb1a175806f3816fd6d89b12c181333a258e1c26
Kaio Delphino <dash.zet@gmail.com>
2022-02-08 12:09:43 -0300

    rename film entity class, fix imports path and calls

--- lib/domain/entities/film_entity.dart
+++ lib/domain/entities/film_entity.dart
@@ -1,6 +1,6 @@
-import 'character.dart';
+import 'character_entity.dart';
 
-class Film {
+class FilmEntity {
   late final String title;
   late final int episodeId;
   late final int id;
@@ -8,12 +8,12 @@ class Film {
   late final String director;
   late final String producer;
   late final DateTime releaseDate;
-  List<Character> _characters = [];
+  List<CharacterEntity> _characters = [];
   late List<String> charactersUrl;
   // late Map _charactersMap = {};
   late final String _url;
 
-  Film(
+  FilmEntity(
     this.title,
     this.episodeId,
     this.id,
@@ -25,7 +25,7 @@ class Film {
     this._url,
   );
 
-  Film.fromMap(Map film) {
+  FilmEntity.fromMap(Map film) {
     title = film['title'] as String;
     episodeId = film['episode_id'] as int;
     openingCrawl = film['opening_crawl'] as String;
@@ -38,12 +38,12 @@ class Film {
     id = int.parse(_urlToList[_urlToList.length - 2]);
   }
 
-  List<Character> get characters => _characters;
+  List<CharacterEntity> get characters => _characters;
 
   // TODO: write better character list fetch
   // film repository will fetch all characters from list
   // and send here so we can display
-  bool setCharacterList(List<Character> charList) {
+  bool setCharacterEntityList(List<CharacterEntity> charList) {
     final listLen = charList.length;
     final _listLen = charactersUrl.length;
     if (_characters == [] && listLen == _listLen) {
