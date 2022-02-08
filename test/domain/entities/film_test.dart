3e806c9a2b0957c2013e6e7570537f7df4ae8cc7
Kaio Delphino <dash.zet@gmail.com>
2022-02-08 01:14:25 -0300

    test: add tests for film entity

---
+++ test/domain/entities/film_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:star_wars_app/domain/entities/film.dart';
+
+import '../../films_response.dart';
+
+void main() {
+  test("Should return a object with a new hope title", () {
+    final films = filmResponse["results"];
+    final film = Film.fromMap(films[0]);
+    expect(film.title, "A New Hope");
+  });
+
+  test("Should return opening crawl as a big string", () {
+    final people = filmResponse["results"];
+    final film = Film.fromMap(people[0]);
+    final openingCrawlLen = film.openingCrawl.length;
+
+    expect((openingCrawlLen > 60), true);
+  });
+
+  test("Should return episodeId of a new hope as 4", () {
+    final people = filmResponse["results"];
+    final film = Film.fromMap(people[0]);
+
+    expect(film.episodeId, 4);
+  });
+
+  test("Should return id of a new hope as 1", () {
+    final people = filmResponse["results"];
+    final film = Film.fromMap(people[0]);
+
+    expect(film.id, 1);
+  });
+
+  test("Should return characters as a list", () {
+    final people = filmResponse["results"];
+    final film = Film.fromMap(people[0]);
+
+    expect(film.characters.runtimeType, List<String>);
+  });
+}
