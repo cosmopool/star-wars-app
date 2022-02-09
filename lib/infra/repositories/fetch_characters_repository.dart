43fd771ee4d6639ab6c76948dc24d21ba1c56471
Kaio Delphino <dash.zet@gmail.com>
2022-02-09 11:51:24 -0300

    feature: implement logic from repositories that fetch data from api

--- lib/infra/repositories/fetch_characters_repository.dart
+++ lib/infra/repositories/fetch_characters_repository.dart
@@ -1,21 +1,30 @@
 import 'package:star_wars_app/core/enums.dart';
+import 'package:star_wars_app/core/mixins/http_error_mixin.dart';
+import 'package:star_wars_app/core/response.dart';
 import 'package:star_wars_app/domain/entities/character_entity.dart';
 import 'package:star_wars_app/domain/repositories/fetch_entities_repository_interface.dart';
-import 'package:star_wars_app/infra/datasource/datasource_interface.dart';
+import 'package:star_wars_app/infra/datasources/api_datasource_interface.dart';
 
-class FetchCharactersRepository implements IFetchEntityRespository {
-  final IDatasource _datasource;
+class FetchCharactersRepository with HttpResponseErrorMenager implements IFetchEntityRespository {
+  final IApiDatasource _datasource;
 
   FetchCharactersRepository(this._datasource);
 
-  List<CharacterEntity> _parseResponse(Map response) {
-    return [];
+  List<CharacterEntity> _treatResponseResult(Map response) {
+    List<CharacterEntity> characterList = [];
+    for (var character in response['results']) {
+      characterList.add(CharacterEntity.fromMap(character));
+    }
+    return characterList;
+  }
+
+  Future<List<CharacterEntity>> fetchDataFromApi() async {
+    final apiResponse = await _datasource(Endpoint.characters);
+    return _treatResponseResult(apiResponse);
   }
 
   @override
-  List<CharacterEntity> call() {
-    final response = _datasource(Endpoint.characters);
-
-    return _parseResponse(response);
+  Future<Response> call() async {
+    return await manageHttpResponse(fetchDataFromApi);
   }
 }
--- lib/infra/repositories/fetch_films_repository.dart
+++ lib/infra/repositories/fetch_films_repository.dart
@@ -1,21 +1,30 @@
 import 'package:star_wars_app/core/enums.dart';
+import 'package:star_wars_app/core/mixins/http_error_mixin.dart';
+import 'package:star_wars_app/core/response.dart';
 import 'package:star_wars_app/domain/entities/film_entity.dart';
 import 'package:star_wars_app/domain/repositories/fetch_entities_repository_interface.dart';
-import 'package:star_wars_app/infra/datasource/datasource_interface.dart';
+import 'package:star_wars_app/infra/datasources/api_datasource_interface.dart';
 
-class FetchFilmsRepository implements IFetchEntityRespository {
-  final IDatasource _datasource;
+class FetchFilmsRepository with HttpResponseErrorMenager implements IFetchEntityRespository {
+  final IApiDatasource _datasource;
 
   FetchFilmsRepository(this._datasource);
 
-  List<FilmEntity> _parseResponse(Map response) {
-    return [];
+  List<FilmEntity> _treatResponseResult(Map response) {
+    List<FilmEntity> characterList = [];
+    for (var character in response['results']) {
+      characterList.add(FilmEntity.fromMap(character));
+    }
+    return characterList;
+  }
+
+  Future<List<FilmEntity>> fetchDataFromApi() async {
+    final apiResponse = await _datasource(Endpoint.characters);
+    return _treatResponseResult(apiResponse);
   }
 
   @override
-  List<FilmEntity> call() {
-    final response = _datasource(Endpoint.films);
-
-    return _parseResponse(response);
+  Future<Response> call() async {
+    return await manageHttpResponse(fetchDataFromApi);
   }
 }
